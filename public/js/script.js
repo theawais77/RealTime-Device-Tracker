31370d532dc6e5821c57f6c3a8ee335e896e6cda
Awais Tahir <theowais456@gmail.com>
2025-09-02 16:49:56 +0500

    Openstreep map

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,27 @@
 const socket=io();
-console.log("hey")
+
+
+if(navigator.geolocation){
+  navigator.geolocation.watchPosition((position)=>{
+    const {latitude,longitude}=position.coords;
+    socket.emit("send-location",{latitude,longitude});
+  },(error)=>{
+    console.error("Error watching position:", error);
+  },
+  {
+    enableHighAccuracy: true,
+    maximumAge: 0,
+    timeout: 5000,
+  }
+);
+}
+const map = L.map("map").setView([0, 0], 12);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: '© OpenStreetMap'
+}).addTo(map);
+
+socket.on("location-updated", (data) => {
+  const { latitude, longitude } = data;
+  L.marker([latitude, longitude]).addTo(map);
+});
