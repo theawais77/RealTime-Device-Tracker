a4747bc615cc47da7615db112ea280eb165a5896
Awais Tahir <theowais456@gmail.com>
2025-09-02 16:34:29 +0500

    Add basic real-time app setup with Express and Socket.io

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require("express");
-const app=express();
+const app = express();
+const http = require("http");
+const path = require("path");
 
-app.get("/",(req,res)=>{
-  res.send("Hello World");
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+// View engine setup
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); // ensure views folder is set
+
+// ✅ Correct way to serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
 });
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.listen(3000,()=>{
-  console.log("Server is running on port 3000");
+server.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
 });
